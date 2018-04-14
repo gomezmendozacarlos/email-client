213b6bbb1e3ac9d64db5f99d792776a34bea1c97
Carlos Gomez <gomezmendozacarlos@gmail.com>
2018-04-14 18:09:56 -0500

    Setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>Email Web Client</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
